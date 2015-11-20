3f3cb8c7b2b9fde955420acc08f0808c52597007
Shuriken (ACER) <tarabet@yandex.ru>
2015-11-20 14:44:00 +0200

    Firebase started to bootstrap

---
+++ src/js/services/firebase.srvc.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Shuriken on 20.11.2015.
+ */
+
+(function() {
+
+    angular
+        .module('appServices')
+        .factory('firebaseSvc', [firebaseSvc]);
+
+    function firebaseSvc() {
+
+
+
+        return {
+
+        }
+    }
+
+})();
